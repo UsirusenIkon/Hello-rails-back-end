00d2b0a36e3e66cab832f77d44ce0c4e63ed1e13
Usirusen Ikon <lexicon048@gmail.com>
2023-03-02 16:14:39 +0100

    Install cors gem

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,16 @@ module HelloRailsBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://127.0.0.1:5173' # replace with the origin of your frontend code
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
